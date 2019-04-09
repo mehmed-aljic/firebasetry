9068640819762bc4e44a5477e2ba9d93288ba252
Mehmed <maljic1@etf.unsa.ba>
2019-04-09 20:14:07 +0200

    firebase tryout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import {FormsModule} from '@angular/forms'
+import { AngularFireModule } from '@angular/fire';
+import { ItemsComponent } from './components/items/items.component';
+import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angularfs'),
+    AngularFirestoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
